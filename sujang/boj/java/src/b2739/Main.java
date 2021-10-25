a3481226ce1d387c180c894dc818d473d5d1659b
bugpigg <bugpigg@gmail.com>
2021-10-26 00:22:33 +0900

    upload class1

---
+++ sujang/boj/java/src/b2739/Main.java
@@ -0,0 +1,18 @@
+package b2739;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		for(int i=1;i<=9;++i) {
+			System.out.println(n+" * "+i+" = "+(n*i));
+			
+		}
+	}
+
+}
