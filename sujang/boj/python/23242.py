3297125c83a51aa4897f955bedf4f07e2e887c68
bugpigg <bugpigg@gmail.com>
2021-11-19 18:47:32 +0900

    upload 1912,23242

--- sujang/boj/python/23242.py
+++ sujang/boj/python/23242.py
@@ -2,22 +2,19 @@ import sys; input = sys.stdin.readline
 
 b = int(input().rstrip())
 n = int(input().rstrip())
+MAX = int(1e9)
 
 sum = [0]*(n+1)
 expo = [0]*(n+1)
-dp = [ [0]*(b+1) for _ in range(n+1)]
+dp = [[MAX]*(b+1) if i != 0 else [0]*(b+1) for i in range(n+1)]
 
 for i in range(1,n+1):
     d = int(input().rstrip())
     sum[i] = sum[i-1] + d
-    expo[i] = expo[i-1] + d**2
-    for j in range(b+1):
-        dp[i][j] = int(1e9)
-
-for i in range(1,n+1):
+    expo[i] = expo[i-1] + (d*d)
     for j in range(1,b+1):
         for k in range(i):
-            if dp[k][j-1] == int(1e9):
+            if dp[k][j-1] == MAX:
                 continue
             avg = (sum[i] - sum[k]) / (i-k)
             error = (expo[i] - expo[k]) - 2 * avg * (sum[i]-sum[k]) + (avg**2) * (i-k)
