5490c045cdbda6887aa3342f0970d8952ea5d744
programmer-ethan <scarlet742@gmail.com>
2021-11-27 17:50:50 +0900

    solve 2042, 11505

--- shnoh/BOJ/Segment_Tree/11505interval_mul.py
+++ shnoh/BOJ/Segment_Tree/11505interval_mul.py
@@ -11,33 +11,35 @@ def init(start, end, index): # tree 만들기
         tree[index] = data[start]
         return tree[index]
     mid = ( start + end ) // 2
-    tree[index] = init(start, mid, index * 2) + init(mid + 1, end, index *2 + 1)
-    return tree[index]
+    tree[index] = init(start, mid, index * 2) * init(mid + 1, end, index *2 + 1)
+    return tree[index] % 1000000007
 
-def interval_sum(start, end, index, left, right):
+def interval_mul(start, end, index, left, right):
     if left > end or right < start:
-        return 0
+        return 1
     if left <= start and right >= end:
         return tree[index]
     mid = (start + end) // 2
-    return interval_sum(start, mid, index * 2, left, right) + interval_sum(mid + 1, end, index * 2 + 1,left, right)
+    return interval_mul(start, mid, index * 2, left, right) * interval_mul(mid + 1, end, index * 2 + 1,left, right)
 
 def update(start, end, index, what, value):
     if what < start or what > end:
         return
-    tree[index] +=value
     if start == end:
+        tree[index] = value
         return
     mid = (start + end) // 2
     update(start, mid, index * 2, what, value)
     update(mid + 1, end, index * 2 + 1, what, value)
+    tree[index] = tree[index * 2] * tree[index * 2 + 1] % 1000000007
 
 init(0, N - 1, 1)
+#print(tree[0:20])
 for _ in range(M + K):
     a, b, c = map(int, sys.stdin.readline().split())
     if a == 1:
-        diff = c - data[b - 1]
         data[b - 1] = c
-        update(0, N - 1, 1, b - 1, diff)
+        update(0, N - 1, 1, b - 1, c)
+        #print(tree[0:20])
     else:
-        print(interval_sum(0, N - 1, 1, b - 1, c - 1))
+        print(interval_mul(0, N - 1, 1, b - 1, c - 1) % 1000000007)
