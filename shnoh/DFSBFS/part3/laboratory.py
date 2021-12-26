91c0cee54cbe9c4045547a0f83822e36a9feb2c4
programmer-ethan <scarlet742@gmail.com>
2021-12-26 15:33:28 +0900

    solve 14502

--- shnoh/DFSBFS/part3/laboratory.py
+++ shnoh/DFSBFS/part3/laboratory.py
@@ -1,6 +1,62 @@
-import sys
-n , m = map(int, sys.stdin.readline().split())
-A = []
+# BOJ 14502
+import sys; input = sys.stdin.readline;
+
+n, m = map(int, input().split())
+data = []
 for i in range(n):
-    A.append(list(map(int, sys.stdin.readline().split())))
-# 일단 경우의 수는 64 * 64 * 64
+    data.append(list(map(int, input().split())))
+#print(A)
+temp = [[0] * m for _ in range(n)] # data를 바꿔가면서
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+max_area = 0
+
+# 안전영역 count. temp를 받아서
+def safety_area():
+    count = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                count += 1
+    return count
+
+# virus 전파 바이러스가...
+def virus_DFS(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and ny >= 0 and nx < n and ny < m and temp[nx][ny] == 0: # n,m방향이 x,y
+            temp[nx][ny] = 2
+            virus_DFS(nx, ny)
+# 모든 경우의 수 64C3
+def solve_DFS(count):
+    global max_area
+
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = data[i][j]
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus_DFS(i, j)
+        max_area = max(max_area, safety_area())
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] = 1
+                count += 1
+                solve_DFS(count)
+                data[i][j] = 0
+                count -= 1
+        
+solve_DFS(0)
+print(max_area)
+
+
+
+
