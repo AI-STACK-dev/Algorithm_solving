2e9430af90f4f860823b5fb5329f0f8dabdcb7bf
programmer-ethan <scarlet742@gmail.com>
2022-01-07 21:34:08 +0900

    solve 1550

---
+++ shnoh/BOJ/BOJ_Step/bronze5/1550.py
@@ -0,0 +1,13 @@
+import sys; input = sys.stdin.readline;
+# A = input().rstrip()
+# n = len(A)
+# res = 0
+# for i in range(n):
+#     k = ord(A[i])
+#     if k >= 65 and k <= 70:
+#         k = k - 55
+#     else:
+#         k = int(A[i])
+#     res += k * 10 ** (n - i - 1)
+# print(res)
+print(int(input(), 16))
