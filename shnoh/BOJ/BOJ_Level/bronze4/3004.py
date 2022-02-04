cb8484e670492d79fb8d8646b1710d4c3d62391f
programmer-ethan <scarlet742@gmail.com>
2022-02-04 12:35:28 +0900

    solve 3004

--- shnoh/BOJ/BOJ_Level/bronze4/3004.py
+++ shnoh/BOJ/BOJ_Level/bronze4/3004.py
@@ -1,7 +1,5 @@
 import sys; input = sys.stdin.readline;
-data = int(input())
-a = data // 2 + 1
-if data % 2 == 1:
-    print(a * (a + 1))
-else:
-    print(a ** 2)
+data = [int(input()) for _ in range(5)]
+a = int(data[0] - data[1] / data[3])
+b = int(data[0] - data[2] / data[4])
+print(min(a,b))
