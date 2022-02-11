7fe87cfcd90db52fd9a6a89dbb702b1d44b472ad
programmer-ethan <scarlet742@gmail.com>
2022-02-11 10:17:14 +0900

    solve 1100

---
+++ shnoh/BOJ/BOJ_Level/bronze2/1100.py
@@ -0,0 +1,8 @@
+import sys; input = sys.stdin.readline;
+data = [input().rstrip() for _ in range(8)]
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if (i + j) % 2 == 0 and data[i][j] == 'F': 
+            cnt += 1
+print(cnt)
