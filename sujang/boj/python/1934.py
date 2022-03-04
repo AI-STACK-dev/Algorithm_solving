398f1cfb74d3f1738669eb4e89fe302399e2697e
bugpigg <bugpigg@gmail.com>
2022-03-05 00:26:36 +0900

    upload 1934.py

---
+++ sujang/boj/python/1934.py
@@ -0,0 +1,11 @@
+num = int(input())
+for i in range(num):
+    a,b = map(int,input().split())
+    A,B = a,b
+    while a != 0:
+        b = b % a
+        a,b = b,a
+         
+    gcd = b
+    lcm = A * B //b
+    print(lcm)
