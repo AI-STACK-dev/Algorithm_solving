a35c059c1026eb2eaa9833833221018973dcd084
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-03-06 17:33:41 +0900

    solve Greedy

---
+++ hjjang/Greedy/무지의먹방라이브/무지의먹방라이브/무지의먹방라이브.cpp
@@ -0,0 +1,75 @@
+﻿// 무지의먹방라이브.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+
+
+bool compare(std::pair<int, int> a, std::pair<int, int> b)
+{
+	return a.second < b.second; 
+}
+
+
+int solution(std::vector<int> food_times, long long k) 
+{
+	if (std::accumulate(food_times.begin(), food_times.end(), 0) <= k)
+		return -1;
+
+	std::priority_queue<std::pair<int, int>>  pq;
+
+	for (int i = 0; i < food_times.size(); i++)
+	{
+		pq.push({ -food_times[i], i + 1 });
+	}
+	//최소힙을 위에 음수 설정 방법 
+
+	
+	/*
+	while (!pq.empty())
+	{
+		std::cout << pq.top().first << ", " << pq.top().second << std::endl;
+		pq.pop();
+	}
+	*/
+	
+	
+	long long summary = 0; //먹기 위해 사용한시간
+	long long previous = 0; //직전에 다먹은 음식시간
+	long long length = food_times.size();
+
+	while (summary + ((-pq.top().first - previous) * length) <= k)
+	{
+		int now = -pq.top().first;
+		pq.pop();
+		summary += (now - previous) * length;
+		length -= 1;
+		previous = now;
+	}
+
+
+	std::vector<std::pair<int, int>> result;
+	while (!pq.empty())
+	{
+		int food_time = -pq.top().first;
+		int num = pq.top().second;
+		pq.pop();
+		result.push_back({ food_time,num });
+	}
+	
+	std::sort(result.begin(), result.end(), compare);
+	return result[(k - summary) % length].second;
+}
+
+
+int main()
+{
+	std::cout << solution({ 8,6,4 }, 15) << std::endl;
+
+	return 0;
+}
+
---
+++ hjjang/Greedy/볼링공고르기/볼링공고르기/볼링공고르기.cpp
@@ -0,0 +1,45 @@
+﻿// 볼링공고르기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+const int Range_Max = 12;
+int ball[Range_Max];
+int result;
+
+int main()
+{
+  
+   /*
+	* 나의 설계 
+	* 배열에 담긴 숫자들이 하나씩 진행 되면서 본인 기준 우측에 있는 숫자들 중 겹치는 개수파악
+	* 이를 누적하며 더해간다.
+	* 단점: 진행 하면서 중복되는 숫자들을 검사하는게 비효율적
+	*/
+
+   /*
+    * 교재 설계
+	* 숫자들의 개수를 미리 파악 어떤 배열에 따로 저장
+	* 천체개수에서 A가정한 숫자를 B는 정할수 없으로 빼주는 방향
+	*/
+
+	int N, M;
+	std::cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		int temp_ball;
+		std::cin >> temp_ball;
+		ball[temp_ball] += 1;
+	}
+
+	for (auto get_ball : ball)
+	{
+		N = N - get_ball;
+		result += get_ball * N;
+	}
+
+	std::cout << result << std::endl;
+	return 0;
+}
+
