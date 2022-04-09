0ca82a5dc8d20502e9d5b8ba4a2733b0e39c473d
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-04-09 23:55:27 +0900

    commit

---
+++ hjjang/boj/boj_1463/boj_1463/boj_1463.cpp
@@ -0,0 +1,35 @@
+﻿// boj_1463.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int result,number,count;
+int ans,temp;
+
+int main()
+{
+    std::cin >> number;
+
+    while (number != count)
+    {
+        ans++;
+        temp = ans;
+        while (temp != 0)
+        {
+            if (temp % 1000 == 666)
+            {
+                count++;
+                break;
+            }
+            else
+            {
+                temp /= 10;
+            }
+        }
+    }
+    std::cout << ans << std::endl;
+    return 0;
+}
+
+
+   
