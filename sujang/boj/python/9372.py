372c7dbbb4a2f4791b4149987532e9b44df5a65f
bugpigg <bugpigg@gmail.com>
2022-04-21 21:12:57 +0900

    upload 9372.py 🐶

---
+++ sujang/boj/python/9372.py
@@ -0,0 +1,8 @@
+import sys;input=sys.stdin.readline
+tc = int(input())
+for _ in range(tc):
+    n,m = map(int,input().split())
+    for __ in range(m):
+        u,v = map(int,input().split())
+    print(n-1)
+    
