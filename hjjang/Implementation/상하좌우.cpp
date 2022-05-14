c06c0178be124f2eea09e8ebb36c97104f8dc248
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-05-14 23:12:10 +0900

    solving class version

---
+++ hjjang/Implementation/상하좌우.cpp
@@ -0,0 +1,65 @@
+﻿// 상하좌우.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+struct Point
+{
+	int y;
+	int x;
+};
+const int MAX = 105;
+int N;
+
+Point user_pos = { 1,1 };
+
+bool IsValid(Point pos)
+{
+	if (pos.x >= 1 && pos.y >= 1 && pos.x <= N && pos.y <= N)
+		return true;
+	else 
+		return false;
+}
+
+void find_destination(std::string dir)
+{
+	if (dir == "R")
+	{
+		user_pos.y++;
+		if (!IsValid(user_pos))
+			user_pos.y--;
+	}
+	else if (dir == "L")
+	{
+		user_pos.y--;
+		if (!IsValid(user_pos))
+			user_pos.y++;
+	}
+	else if (dir == "U")
+	{
+		user_pos.x--;
+		if (!IsValid(user_pos))
+			user_pos.x++;
+	}
+	else if (dir == "D")
+	{
+		user_pos.x++;
+		if (!IsValid(user_pos))
+			user_pos.x--;
+	}
+}
+
+int main()
+{
+	std::cin >> N;
+
+	for (int i = 0; i < 6; ++i)
+	{
+		std::string dir_str;
+		std::cin >> dir_str;
+		find_destination(dir_str);
+	}
+	std::cout << user_pos.x << " " << user_pos.y << std::endl;
+	return 0;
+}
+
---
+++ hjjang/Implementation/시각.cpp
@@ -0,0 +1,50 @@
+﻿// 시각.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+class Time
+{
+private:
+    int m_hour;
+    int m_minute;
+    int m_second;
+    int m_answer;
+public:
+    Time(int hour, int minute, int second) : m_hour(hour), m_minute(minute), m_second(second),m_answer(0)
+    {};
+
+    int Find_3_Counter()
+    {
+        for (int i = 0; i < m_hour + 1; ++i)
+        {
+            for (int j = 0; j < m_minute; ++j)
+            {
+                for (int k = 0; k < m_second; ++k)
+                {
+                    std::string time_str = std::to_string(i) + std::to_string(j) + std::to_string(k);
+                    size_t npos = time_str.find("3");
+                    if (npos != std::string::npos)
+                    {
+                        m_answer++;
+                    }
+                }
+            }
+        }
+        return m_answer;
+    }
+    ~Time()
+    {}
+};
+
+int main()
+{
+    int N;
+    std::cin >> N;
+
+    Time time(N, 60, 60);
+    std::cout << time.Find_3_Counter() << std::endl;
+
+    return 0;
+}
+
