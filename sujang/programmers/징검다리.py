116d4e2fa47a5c6acf80e05d876a11eb78ee53ec
bugpigg <bugpigg@gmail.com>
2022-05-21 23:27:09 +0900

    upload 징검다리.py

---
+++ sujang/programmers/징검다리.py
@@ -0,0 +1,27 @@
+def check_possible(stones,k,x):
+    prev = 0
+    for s in stones:
+        # 밟을 수 있는 돌
+        if s >= x:
+            if prev >= k:
+                return False
+            prev = 0
+        else:
+            prev += 1
+    # exit condition
+    if prev >= k:
+        return False
+    return True
+            
+def binary_search(stones,k):
+    lo,hi = 1,200000000
+    while lo+1 < hi:
+        mid = (lo+hi)//2
+        if check_possible(stones,k,mid):
+            lo = mid
+        else:
+            hi = mid
+    return lo
+def solution(stones, k):
+    return binary_search(stones,k)a
+    
