8f5acec567164cb4e0a22786521d96364adf1cdd
bugpigg <bugpigg@gmail.com>
2022-05-23 23:47:55 +0900

    update wc_294 - 4.py

--- sujang/leet/WC_294/4.py
+++ sujang/leet/WC_294/4.py
@@ -1,42 +1,33 @@
+from itertools import accumulate
+
 class Solution:
-    def tree_init(self, tree, idx, start, end, data):
-        if start == end:
-            tree[0][idx] = data[start]
-            tree[1][idx] = data[start]
-            return tree[0][idx],tree[1][idx]
-        mid = (start+end)//2
-        a,b = self.tree_init(tree, idx*2, start, mid, data)
-        c,d = self.tree_init(tree,(idx*2)+1, mid+1, end, data)
-        tree[0][idx] = min(a,c)
-        tree[1][idx] = b + d
-        return tree[0][idx],tree[1][idx]
+    def totalStrength(self, A):
+        mod = 10 ** 9 + 7
+        n = len(A)
 
-    def tree_query(self, tree, idx, start, end, left, right):
-        MAX = int(1e9)+1
-        if end < left or start > right:
-            return  [MAX,0]
-        if left <= start and right >= end:
-            return tree[0][idx],tree[1][idx]
-        mid = (start+end)//2
-        a,b = self.tree_query(tree, idx*2, start, mid, left, right)
-        c,d = self.tree_query(tree, (idx*2)+1, mid+1, end, left, right)
-        return min(a,c), b+d
-
-    def totalStrength(self, s) -> int:
-        n = len(s)
-        segTree = [[0]*(n*4+1) for _ in range(2)]
-
-        # Tree Init
-        self.tree_init(segTree, 1, 0, n-1, s)
-
-        ans = 0
-        # Tree Query
+        right = [n] * n
+        stack = []
         for i in range(n):
-            for j in range(i,n):
-                MIN,SUM = self.tree_query(segTree, 1, 0, n-1, i, j)
-                ans += MIN*SUM
-        
-        return ans%(int(1e9)+7)
+            while stack and A[stack[-1]] > A[i]:
+                right[stack.pop()] = i
+            stack.append(i)
+
+        left = [-1] * n
+        stack = []
+        for i in range(n-1, -1, -1):
+            while stack and A[stack[-1]] >= A[i]:
+                left[stack.pop()] = i
+            stack.append(i)
+
+        res = 0
+        acc = list(accumulate(accumulate(A), initial = 0))
+        for i in range(n):
+            l, r = left[i], right[i]
+            lacc = acc[i] - acc[max(l, 0)]
+            racc = acc[r] - acc[i]
+            ln, rn = i - l, r - i
+            res += A[i] * (racc * ln - lacc * rn) % mod
+        return res % mod
 
 
 a = [1,3,1,2]
