da7dc3edaad2d5141c2d5fc2eb20f1f67eec58ca
programmer-ethan <scarlet742@gmail.com>
2022-09-10 21:58:48 +0900

    Create 9325.py

---
+++ shnoh/BOJ/BOJ_Level/bronze3/9325.py
@@ -0,0 +1,9 @@
+import sys; input = sys.stdin.readline;
+t = int(input())
+for _ in range(t):
+    s = int(input())
+    n = int(input())
+    for _ in range(n):
+        q, p = map(int, input().split())
+        s += q * p
+    print(s)
