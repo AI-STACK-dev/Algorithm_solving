96872f4231fe3c67e88aaf4fd92b78bc466adb9a
programmer-ethan <scarlet742@gmail.com>
2022-11-04 20:40:14 +0900

    Create 10214.py

---
+++ shnoh/BOJ/BOJ_Level/bronze3/10214.py
@@ -0,0 +1,14 @@
+import sys; input = sys.stdin.readline;
+t = int(input())
+for _ in range(t):
+    Y = K = 0
+    for _ in range(9):
+        y, k = map(int, input().split())
+        Y += y
+        K += k
+    if Y > K:
+        print("Yonsei")
+    elif Y < K:
+        print("Korea")
+    else:
+        print("Draw")
